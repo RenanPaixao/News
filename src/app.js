cf9ccd81a4df6451a70aeed66e99a54705889ecd
Renan Paixão <57810270+RenanPaixao@users.noreply.github.com>
2021-09-04 15:51:42 -0300

    feat: agora adiciona o email ou o atualiza

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
 import mailchimp from '@mailchimp/mailchimp_marketing';
+import express from 'express';
+import md5 from 'md5';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const listID = process.env.LIST_ID;
 
 mailchimp.setConfig({
   apiKey: process.env.API_KEY,
   server: 'us5',
 });
 
-async function run() {
-  const response = await mailchimp.ping.get();
-  console.log(response);
-}
+app.put('/add', async (req, res) => {
+  try {
+    await mailchimp.lists.setListMember(
+      listID,
+      md5(req.body.email.toLowerCase()),
+      {
+        email_address: req.body.email,
+        status_if_new: 'subscribed',
+        merge_fields: {},
+      }
+    );
+    res.status(200).send('Adicionado com sucesso!');
+  } catch (e) {
+    res
+      .status(405)
+      .send({ error: 'Ocorreu um erro ao adicionar. Tente novamente!' });
+  }
+});
 
-run();
+export default app;
